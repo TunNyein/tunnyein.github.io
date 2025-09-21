2e1994b4bf1803837ae9c4a849f5be9083b0b5a4
Tun Nyein <tunnyein.ict@gmail.com>
2025-09-21 10:48:03 +0630

    update index.html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Update year dynamically
-document.getElementById("year").textContent = new Date().getFullYear();
+// // Update year dynamically
+// document.getElementById("year").textContent = new Date().getFullYear();
 
-// Smooth scroll for nav links
-document.querySelectorAll('.navbar a').forEach(link => {
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if(target) target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
+// // Smooth scroll for nav links
+// document.querySelectorAll('.navbar a').forEach(link => {
+//   link.addEventListener('click', function(e){
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     if(target) target.scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
